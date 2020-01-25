1c2d1f140e73b66490484c8e3bb9c7c632d6148f
Taishawn King <taishawnking1@yahoo.com>
2020-01-25 05:46:25 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,7 +1,6 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 class Kid
-  Kid.metadata
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
 
